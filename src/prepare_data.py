e595c32a55a44274c3667c9aa4cd4b9132fd0330
Shatunov Evgeny <shatunov868@gmail.com>
2025-02-09 11:49:27 +0300

    feat: Create prepare_data function
    The function initializes CIFAR10 test and train data with normalized tensors.

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,4 +1,7 @@
 from torchvision.datasets import CIFAR10
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from typing import Tuple
 
 
 def download_data() -> None:
@@ -10,6 +13,36 @@ def download_data() -> None:
     CIFAR10("../data/CIFAR10/train", download=True)
     CIFAR10("../data/CIFAR10/test", download=True)
     return
+
+
+def prepare_data() -> Tuple[Dataset, Dataset]:
+    """
+    Initialize CIFAR10 test and train data with normalized tensors.
+
+    :return: Tuple with train and test data as torchvision.datasets.
+    """
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
+
+    train_dataset = CIFAR10(
+        root="CIFAR10/train",
+        train=True,
+        transform=transform,
+        download=False,
+    )
+
+    test_dataset = CIFAR10(
+        root="CIFAR10/test",
+        train=False,
+        transform=transform,
+        download=False,
+    )
+
+    return train_dataset, test_dataset
 
 
 if __name__ == "__main__":
