d7be3771d127c7b714c9557cb0e8c92cf7f3ff26
Shatunov Evgeny <shatunov868@gmail.com>
2025-02-22 22:27:29 +0300

    fix: Delete tmpqdir in test_save_model

--- test/test_train.py
+++ test/test_train.py
@@ -161,11 +161,9 @@ def generate_dummy_model():
     return model
 
 
-def test_save_model(tmpdir):
+def test_save_model():
     model = generate_dummy_model()
-    path = os.path.join(
-        tmpdir, "model.pt"
-    )  # Path to save the model weights (in the temporary directory)
+    path = "weights/model.pt"  # Path to save the model weights
 
     # Mock wandb to avoid actual wandb calls during testing
     with patch("wandb.run") as mock_wandb_run:
