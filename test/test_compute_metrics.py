49434c8b08ff75c9b63186536eb2b5ddce6ea21f
Shatunov Evgeny <shatunov868@gmail.com>
2025-02-22 22:29:53 +0300

    test: Create test for compute_metrics

---
+++ test/test_compute_metrics.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+
+from src.compute_metrics import compute_metrics
+
+
+def test_compute_metrics():
+    compute_metrics()
+    path = "final_metrics.json"
+    assert os.path.exists(path)
+
+    with open(path, "r") as f:
+        metrics = json.load(f)
+    assert metrics["accuracy"] is not float
